adf6bdb153ea5e2a159f1bb51f6b63eebf9af3e0
andieelmes <andie.elmes@gmail.com>
2023-05-05 11:48:21 +0500

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line import/prefer-default-export
 export const askName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-}
+};
